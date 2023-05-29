e0db01d88fa176fc60a58347168b9ca2030510d3
Lubos Sosna <lubossosna@seznam.cz>
2023-05-29 17:16:42 +0200

    update record fce

--- app_pojistovna/views.py
+++ app_pojistovna/views.py
@@ -87,6 +87,19 @@ def add_record(request):
 
 
 
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Zaznam byl uspesne upraven")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request, "Musite by prihlaseni")
+        return redirect('home')
+
 
 
 
